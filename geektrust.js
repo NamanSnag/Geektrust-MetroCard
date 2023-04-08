def2f8f03f97d0b629d37c75d3b9ba82ec091795
NamanSnag <nnag44141@gmail.com>
2023-04-08 13:13:10 +0530

    added print summary function

--- geektrust.js
+++ geektrust.js
@@ -45,4 +45,43 @@ inputLines.forEach((data, index) => {
   }
 });
 
-console.log(metroCard);
+// console.log(metroCard);
+
+// central station object
+// stoe output summary of central station
+const centralStation = {
+  station: 'CENTRAL',
+  totalCollection : 0,
+  discount : 0,
+  noOfSenior : 0,
+  noOfAdult : 0,
+  noOfKid : 0,
+}
+
+// airport station object
+// stoe output summary of airport station
+const airportStation = {
+  Station: 'AIRPORT',
+  totalCollection : 0,
+  discount : 0,
+  noOfSenior : 0,
+  noOfAdult : 0,
+  noOfKid : 0,
+}
+
+//  ------------------- We have to add the properties of station objects here -----------------
+
+
+// printing function for station summary
+const printSummary = (data) => {
+  const {Station, totalCollection, discount, noOfSenior, noOfAdult, noOfKid} = data;
+  console.log("TOTAL_COLLECTION"+"  "+Station+"  "+totalCollection+"  "+discount);
+  console.log("PASSENGER_TYPE_SUMMARY")
+  if(noOfAdult > 0) console.log("ADULT "+ noOfAdult)
+  if(noOfKid > 0) console.log("KID "+ noOfKid)
+  if(noOfSenior > 0) console.log("SENIOR_CITIZEN "+ noOfSenior)
+}
+
+// Print station details summary
+printSummary(centralStation);
+printSummary(airportStation);
